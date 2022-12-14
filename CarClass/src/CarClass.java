4eaafa515583f3b59649999074a2b0e22ddc87d9
LeylaMolnar <leyla.molnar24@gmail.com>
2022-12-14 19:47:23 +0200

    Imported code
    Imported code for the CarClass app that I had made previously

--- CarClass/src/CarClass.java
+++ CarClass/src/CarClass.java
@@ -1,5 +1,58 @@
+
 public class CarClass {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+	public static void main(String[] args) {
+		Car car1 = new Car("Fiat", "500", 50);
+		car1.printData();
+		car1.accelerate();
+		car1.brake();
+		car1.printData();
+		car1.refuel(15);
+	}
+}
+
+class Car {
+
+	private String brand;
+	private String model;
+	private int amountOfFuel;
+
+
+	public void brake() {
+		System.out.println("Car is braking");
+	}
+
+	public void accelerate() {
+
+		if (amountOfFuel > 0) {
+			System.out.println("Car is accelerating");
+			amountOfFuel--;
+		} else {
+			System.out.println("Out of fuel");
+		}
+	}
+
+	public void printData() {
+		System.out.println("Brand: " + brand + "\tModel: " + model + "\tAmount of fuel: " + amountOfFuel);
+	}
+
+	public void refuel(int amount) {
+		amountOfFuel += amount;
+		System.out.println("Fuel in the tank: " + (amountOfFuel - amount) + "\tRefuel: " + amount
+				+ "\tFuel in the tank after refuel: " + amountOfFuel);
+
+	}
+
+
+	public Car(String brand, String model, int amountOfFuel) {
+		this.brand = brand;
+		this.model = model;
+		this.amountOfFuel = amountOfFuel;
+	}
+
+
+	public Car() {
+
+	}
+
 }
